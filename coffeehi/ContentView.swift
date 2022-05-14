1d88ed758d53b9e99b658907d7c7b145de4e4e56
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-14 00:49:07 -0500

    Initial Commit

---
+++ coffeehi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffeehi
+//
+//  Created by Lyndi Castrejon on 5/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coffeehi/coffeehiApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeehiApp.swift
+//  coffeehi
+//
+//  Created by Lyndi Castrejon on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct coffeehiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
