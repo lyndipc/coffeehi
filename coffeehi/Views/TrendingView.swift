54965c53b539acf39abdadb07db2c5bbe6e752c9
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-19 13:37:56 -0500

    Create TrendingView

---
+++ coffeehi/Views/TrendingView.swift
@@ -0,0 +1,78 @@
+//
+//  TrendingView.swift
+//  coffeehi
+//
+//  Created by Lyndi Castrejon on 5/19/22.
+//
+
+import SwiftUI
+
+struct TrendingView: View {
+    
+    var body: some View {
+        
+        VStack {
+            
+            LogoHeader()
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                
+                VStack(alignment: .leading, spacing: 10.0) {
+                    Text("Trending")
+                        .bold()
+                        .font(.title2)
+                    
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("Modern French Press")
+                        Text("Clean Coffee Setups")
+                        Text("Show Off Your Mugs!")
+                        Text("Latte Art")
+                    }
+                    .padding(.leading)
+                    
+                    Spacer()
+                    
+                    Text("Top Posts")
+                        .bold()
+                        .font(.title2)
+                    
+                    HStack {
+                        Rectangle()
+                            .fill(Color(lightGray))
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(5)
+                        
+                        Rectangle()
+                            .fill(Color(lightGray))
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(5)
+                    }
+                    
+                    HStack {
+                        Rectangle()
+                            .fill(Color(lightGray))
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(5)
+                        
+                        Rectangle()
+                            .fill(Color(lightGray))
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(5)
+                    }
+                
+                    Spacer()
+                }
+            }
+            .padding(.top)
+            .foregroundColor(Color(primaryColor))
+        }
+    }
+}
+
+struct TrendingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrendingView()
+    }
+}
