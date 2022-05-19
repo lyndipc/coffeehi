135dee0689c619c1a0957c8fbd49021ddc6f9dc9
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-19 11:11:12 -0500

    Rename ContentView to LaunchView. Add relevant components/comments to LaunchView

--- coffeehi/Views/LaunchView.swift
+++ coffeehi/Views/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  coffeehi
 //
 //  Created by Lyndi Castrejon on 5/14/22.
@@ -9,19 +9,18 @@ import SwiftUI
 
 let primaryColor = UIColor(red: 0.016, green: 0.767, blue: 0.541, alpha: 1)
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         VStack {
-            NavBar()
             
-            Spacer()
-            Text("hi")
+            // TODO: Authenticate user prior to showing FeedView
+            FeedView()
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
--- coffeehi/coffeehiApp.swift
+++ coffeehi/coffeehiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct coffeehiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
