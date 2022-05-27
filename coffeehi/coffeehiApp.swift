26eb8acdf729621dac825e58d537d6e21f47f561
Lyndi Castrejon <lyndic@protonmail.com>
2022-05-27 18:32:30 -0500

    Initial Firebase setup. Add GoogleService-Info.plist to .gitignore

--- coffeehi/coffeehiApp.swift
+++ coffeehi/coffeehiApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
 @main
 struct coffeehiApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LaunchView()
