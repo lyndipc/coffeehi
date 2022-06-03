21cdc02209fdbf5770a3f829a6fa06bf55c89571
Lyndi Castrejon <lyndic@protonmail.com>
2022-06-03 01:35:22 -0500

    Reorganize file structure. Create EditProfileView

---
+++ coffeehi/Views/Profile/EditProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  EditProfileView.swift
+//  coffeehi
+//
+//  Created by Lyndi Castrejon on 6/3/22.
+//
+
+import SwiftUI
+
+// TODO: Create profile editor view for sheet
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // Change profile image
+        
+        // Update bio
+        
+        // Save data button
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
